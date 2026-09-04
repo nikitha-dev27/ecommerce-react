aa94de90c3d4baff523b8e20db6780a8717889e5
Nikitha <mnikitha2727@gmail.com>
2026-09-04 15:07:28 +0530

    Resolve Vite config conflict

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/ecommerce-react/",
-=======
->>>>>>> a306216 (Fix Render port configuration)
 });
